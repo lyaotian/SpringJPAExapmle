52474b931181eacc5b6c1bd3adcb3a35ca29514d
Yaotian Leung <lyaotian@live.com>
2013-07-20 00:36:28 +0800

    init

---
+++ src/main/java/com/lyaotian/example/pojo/Email.java
@@ -0,0 +1,72 @@
+package com.lyaotian.example.pojo;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * User: Yaotian Leung
+ * Date: 7/19/13
+ * Time: 10:51 PM
+ */
+@Entity
+public class Email {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private long id;
+    private String email;
+    @ManyToMany(mappedBy = "emails")
+    private Set<Person> persons = new HashSet<Person>();
+
+    public Email(){
+
+    }
+
+    public Email(String email) {
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Email email1 = (Email) o;
+
+        if (id != email1.id) return false;
+        if (!email.equals(email1.email)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + email.hashCode();
+        return result;
+    }
+
+    public Set<Person> getPersons() {
+        return persons;
+    }
+
+    public void setPersons(Set<Person> persons) {
+        this.persons = persons;
+    }
+}
---
+++ src/main/java/com/lyaotian/example/pojo/Person.java
@@ -0,0 +1,84 @@
+package com.lyaotian.example.pojo;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * User: Yaotian Leung
+ * Date: 7/19/13
+ * Time: 10:49 PM
+ */
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private boolean isFemale;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date birthday;
+    @ManyToMany
+    private Set<Email> emails = new HashSet<Email>();
+
+    public Person(){
+
+    }
+
+    public Person(String firstName, String lastName, boolean female, Date birthday) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        isFemale = female;
+        this.birthday = birthday;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public boolean isFemale() {
+        return isFemale;
+    }
+
+    public void setFemale(boolean female) {
+        isFemale = female;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Set<Email> getEmails() {
+        return emails;
+    }
+
+    public void setEmails(Set<Email> emails) {
+        this.emails = emails;
+    }
+}
